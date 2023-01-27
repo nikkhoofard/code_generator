1705d530c0ff5cb18ac4c2b9b6681c35da6e4e62
nikkhoofard <nikkhoofard2000@gmail.com>
2023-01-28 01:02:56 +0330

    creat swich for get password from user

--- scripts/main.py
+++ scripts/main.py
@@ -1,11 +1,9 @@
-
-
-import string
+import argparse
 import string
 from random import choices
 
 
-def create_password(lengh=8, upper=False, lower=False, digit=False, punc=False):
+def create_password(length=8, upper=False, lower=False, digit=False, punc=False):
     pool = ''
     if upper:
         pool += string.ascii_uppercase
@@ -18,13 +16,21 @@ def create_password(lengh=8, upper=False, lower=False, digit=False, punc=False):
     if pool == '':
         pool += string.ascii_lowercase
 
-    password = choices(pool, k=lengh)
+    password = choices(pool, k=length)
     return ''.join(password)
 
 
 if __name__ == '__main__':
-    print(create_password())
-    print(create_password(18, digit=True))
-    print(create_password(digit=True, punc=True, lower=True))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('length', type=int, help="length of password")
+    parser.add_argument('-u', '--upper', help='upper case', action='store_true')
+    parser.add_argument('-l', '--lower', help="lowercase", action='store_true')
+    parser.add_argument('-d', '--digit', help="digit", action='store_true')
+    parser.add_argument('-p', '--punc', help="punc", action='store_true')
+
+    args = parser.parse_args()
+
+    print(create_password(args.length, args.upper, args.lower, args.digit, args.punc))
+
 
 
